423c26429ced920a8c590fd80d60d68cb26ac0fc
dfd <dfd@justdfd.com>
2018-10-26 09:40:33 -0400

    Initial Commit

---
+++ CoreImageFilters/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreImageFilters
+//
+//  Created by Dave Dombrowski on 10/26/18.
+//  Copyright © 2018 justDFD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
